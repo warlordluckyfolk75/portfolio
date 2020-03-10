78da72d68d54bb367d365fbb3311d9b2bd6680e1
lu <chigbulucky@gmail.com>
2020-03-10 15:58:01 +0100

    portfolio

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+// $('#navbar a,btn').on('click'),function(event){
+    // if ("this.hash !== "){
+        // event.preventDefault();
+    // const hash = this.hash;
+    // $('html,body').animate(
+        // {
+            // scrolltop: $(hash).offset().top - 80
+        // },
+        // 800
+    // );
+    // }
+// };
